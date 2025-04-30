fdba7b30507aa79d1d383fd33d891196f876bb2f
Santiago Bonoli <santiagofbonoli@gmail.com>
2025-04-30 16:37:21 -0300

    fixed vol calc.

--- volumeoverlapcalc.f90
+++ volumeoverlapcalc.f90
@@ -7,7 +7,7 @@ use system
 use chainsdat
 use MPI, only : rank
 implicit none
-real*8 vect1T(3), vect1R(3),vect2T(3),vect2R(3)
+real*8 vect1R(3),vect1T(3),vect2T(3),vect2R(3)
 integer j, k, ir,it,ip, ix, iy, iz, nn
 real*8 x,y,z, vect2
 real*8, external :: pos2x, pos2y, pos2z
@@ -27,78 +27,65 @@ dphi = pi/float(Nphi)
 Nradius = 200
 rlig = 0.088*(long+1)+0.525
 dradius = rlig/float(Nradius)
-sum = 0.0
 
+if(rank.eq.0) then
+	open(unit=46, file= 'volume_overlap.dat', status='replace', action='write')
+endif
 do j = 1, NNN
-	do k = 1, NNN
-		if (k.ge.j) then
-		rA = Aell(1,j)
-		rB = Aell(1,k)
+	sum = 0.0
+	rA = Aell(1,j)
+	vect1T(1) = pos2x(j,0) 
+	vect1T(2) = pos2y(j,0)
+	vect1T(3) = pos2z(j,0)
+	vect1R = MATMUL(IMAT,vect1T) ! coordinates of particle in real space
+	do ir = 1, Nradius
+	radius = float(ir)*dradius + rA
+	do it = 1, Ntita
+	tita = dtita*float(it-1)
+	do ip = 1, Nphi
+		phi = dphi*float(ip-1)
 
-		vect1T(1) = pos2x(j,0) 
-		vect1T(2) = pos2y(j,0)
-		vect1T(3) = pos2z(j,0)
-		vect1R = MATMUL(IMAT,vect1T) ! coordinates of particle in real space
+		x = radius*sin(phi)*cos(tita) + vect1R(1)
+		y = radius*sin(phi)*sin(tita) + vect1R(2)
+		z = radius*cos(phi) + vect1R(3)
 
-		do ir = 1, Nradius
-		radius = float(ir)*dradius + rA
-		do it = 1, Ntita
-		tita = dtita*float(it-1)
-		do ip = 1, Nphi
-			phi = dphi*float(ip-1)
-
-			x = radius*sin(phi)*cos(tita) + vect1R(1)
-			y = radius*sin(phi)*sin(tita) + vect1R(2)
-			z = radius*cos(phi) + vect1R(3)
-
-			flag = 0
-
+		flag = 0
+		do k = 1, NNN
 			do ix = -nn, nn
 			do iy = -nn, nn
 			do iz = -nn, nn
-
+			rB = Aell(1,k)
 			vect2T(1) = pos2x(k,ix) 
 			vect2T(2) = pos2y(k,iy)
 			vect2T(3) = pos2z(k,iz)
 			vect2R = MATMUL(IMAT,vect2T) ! coordinates of particle in real space
-			
-			if(j.eq.k) then
-			if((ix.ne.0).or.(iy.ne.0).or.(iz.ne.0)) then
+			if((j.ne.k).or.(ix.ne.0).or.(iy.ne.0).or.(iz.ne.0)) then
 				vect2 = (vect2R(1) - x)**2 + (vect2R(2) - y)**2 + (vect2R(3) - z)**2
-				r2 = (rlig+rA)**2
+				r2 = (rlig+rB)**2
 				if(vect2.lt.r2) then
 	  				flag = 1
 	  				exit
 	  			endif
-  			endif
-  			else
-	  			vect2 = (vect2R(1) - x)**2 + (vect2R(2) - y)**2 + (vect2R(3) - z)**2
-				r2 = (rA+rlig)**2
-				if(vect2.lt.r2) then
-	  				flag = 1
-	  				exit
-  				endif
-  			endif
-
+	  		endif
 			enddo
 			if(flag.eq.1)exit
 			enddo
 			if(flag.eq.1)exit
 			enddo
+			if(flag.eq.1)exit
+		enddo
+		if(flag.eq.1)sum = sum + radius*radius*sin(phi)*dtita*dphi*dradius
+		enddo
+		enddo
+		enddo
 
-			if(flag.eq.1) sum = sum + radius*radius*sin(phi)*dtita*dphi*dradius
-		enddo
-		enddo
-		enddo
-	end if
-	enddo
+	if(rank.eq.0) then
+	write(46,*) sum
+	endif
+
 enddo
 
-if(rank.eq.0) then
-open(unit=46, file= 'volume_overlap.dat', status='replace', action='write')
-write(46,*) sum
 close(46)
-endif
 
 end subroutine
 
