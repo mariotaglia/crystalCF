Fixed lenght value. Added plots vs ratio of radius (alpha). Changed export, now the DF for part1, part2 and binary export as KT/particle or KT/salt_particle. The Normalization from MD data is only in DF global, and binary plots.

scripts_python/calculate_energy.py
@@ -13,7 +13,7 @@ def mean_al(df):
         'F_norm': 'mean'
     })
 
-def estimate_part_F(part, part_cell, factor_aL_part, ni, k_part, n1, n2, gen_curves_flag, k_reflex_part):
+def estimate_part_F(part, part_cell, factor_aL_part, ni, k_part, gen_curves_flag, k_reflex_part):
     F_norm = []
     csv_file = [f"{part}_results_output.csv", f"{part}_references_output.csv"]
     data_part = pd.read_csv(csv_file[0], skiprows=0)
@@ -34,13 +34,19 @@ def estimate_part_F(part, part_cell, factor_aL_part, ni, k_part, n1, n2, gen_cur
     x_cell =  np.arange(aL_cell[0], aL_cell[-1], 0.001)
     y_cell = CubicSpline(aL_cell, F_norm_cell)(x_cell)
 
-    F_min_cell = y_cell.min()/(n1+n2)
+    # Ajuste cúbico (polinomio de grado 3)
+    coeficientes = np.polyfit(aL_cell, F_norm_cell, 4)
+    polinomio = np.poly1d(coeficientes)
+
+    # Evaluación del polinomio ajustado
+    y_cell = polinomio(x_cell)
+    F_min_cell = y_cell.min()
     aL_min_cell = x_cell[y_cell.argmin()]
 
     if gen_curves_flag == True:
         fig_sub, ax_sub = plt.subplots()
-        ax_sub.scatter(aL_cell, F_norm_cell/(n1+n2))
-        ax_sub.plot(x_cell,y_cell/(n1+n2))
+        ax_sub.scatter(aL_cell, F_norm_cell)
+        ax_sub.plot(x_cell,y_cell)
         ax_sub.set_xlabel(r'a$_{\text{L}}$',fontsize=14)
         ax_sub.set_ylabel(r'$\Delta$F (k$_{\text{B}}$T)',fontsize=14)
         ax_sub.axvline(aL_min_cell,ls='--',c='darkgray',zorder=-1)
@@ -48,7 +54,7 @@ def estimate_part_F(part, part_cell, factor_aL_part, ni, k_part, n1, n2, gen_cur
         plt.close(fig_sub)
 
         fig_sub, ax_sub = plt.subplots()
-        ax_sub.scatter(aL_cell, F_tot/(n1+n2))
+        ax_sub.scatter(aL_cell, F_tot)
         ax_sub.set_xlabel(r'a$_{\text{L}}$',fontsize=14)
         ax_sub.set_ylabel(r'$\Delta$F (k$_{\text{B}}$T)',fontsize=14)
         ax_sub.axvline(aL_min_cell,ls='--',c='darkgray',zorder=-1)
@@ -57,7 +63,7 @@ def estimate_part_F(part, part_cell, factor_aL_part, ni, k_part, n1, n2, gen_cur
 
     return aL_min_cell, F_min_cell, x_cell
 
-def estimate_part_contrib(part, part_cell, factor_aL_part, ni, k_part, n1, n2, F, aL_array, aL_min, gen_curves_flag, k_reflex_part):
+def estimate_part_contrib(part, part_cell, factor_aL_part, ni, k_part, F, aL_array, aL_min, gen_curves_flag, k_reflex_part):
     F_cell = []
     for f_name in F:
         csv_file = [f"{part}_results_output.csv", f"{part}_references_output.csv"]
@@ -78,13 +84,18 @@ def estimate_part_contrib(part, part_cell, factor_aL_part, ni, k_part, n1, n2, F
 
     F_norm_cell = np.sum(F_cell, axis=0)
     y_cell = CubicSpline(aL_cell, F_norm_cell)(aL_array)
-    F_min_cell = y_cell[aL_array==aL_min]/(n1+n2)
+    
+    coeficientes = np.polyfit(aL_cell, F_norm_cell, 4)
+    polinomio = np.poly1d(coeficientes)
+    y_cell = polinomio(aL_array)
+
+    F_min_cell = y_cell[aL_array==aL_min]
     F_calc = F_min_cell[0]
 
     if gen_curves_flag == True:
         fig_sub, ax_sub = plt.subplots()
-        ax_sub.scatter(aL_cell, F_norm_cell/(n1+n2))
-        ax_sub.plot(aL_array,y_cell/(n1+n2))
+        ax_sub.scatter(aL_cell, F_norm_cell)
+        ax_sub.plot(aL_array,y_cell)
         ax_sub.axvline(aL_min,ls='--',c='darkgray',zorder=-1)
         ax_sub.set_xlabel(r'a$_{\text{L}}$',fontsize=14)
         if "F_trans" in F:
@@ -122,7 +133,11 @@ def estimate_bin_F(name, factor_bcell, k_bin, n1, n2, ax, gamma, gen_curves_flag
     x_bin =  np.arange(aL_bin[0], aL_bin[-1], 0.001)
     y_bin = CubicSpline(aL_bin, F_norm_bin)(x_bin)
 
-    F_min_bin = y_bin.min()/(n1+n2)
+    coeficientes = np.polyfit(aL_bin, F_norm_bin, 4)
+    polinomio = np.poly1d(coeficientes)
+    y_bin = polinomio(x_bin)
+
+    F_min_bin = y_bin.min()
     aL_min_bin = x_bin[y_bin.argmin()]
 
     if gen_curves_flag == True:
@@ -178,7 +193,11 @@ def estimate_bin_contrib(name, factor_bin_cell, k_bin, n1, n2, F, aL_array, aL_m
     F_norm_bin = np.sum(F_bin, axis=0)
     y_bin = CubicSpline(aL_bin, F_norm_bin)(aL_array)
 
-    F_min_bin = y_bin[aL_array==aL_min]/(n1+n2)
+    coeficientes = np.polyfit(aL_bin, F_norm_bin, 4)
+    polinomio = np.poly1d(coeficientes)
+    y_bin = polinomio(aL_array)
+
+    F_min_bin = y_bin[aL_array==aL_min]
     F_calc = F_min_bin[0]
 
     if gen_curves_flag == True:
@@ -203,9 +222,9 @@ def estimate_bin_contrib(name, factor_bin_cell, k_bin, n1, n2, F, aL_array, aL_m
 
 def delta_energy_F(dict_delta, cell, n1, n2):
     df_delta = pd.DataFrame.from_dict(dict_delta)
-    F_part1 = (df_delta.loc[df_delta["#part"] == "part1", "ΔF_min"]*n1).tolist()
-    F_part2 = (df_delta.loc[df_delta["#part"] == "part2", "ΔF_min"]*n2).tolist()
-    F_min_bin = df_delta.loc[df_delta["#part"] == "binary", "ΔF_min"].values[0]  # Tomar único valor binario
+    F_part1 = (df_delta.loc[df_delta["#part"] == "part1", "ΔF_min"]*n1/(n1+n2)).tolist()
+    F_part2 = (df_delta.loc[df_delta["#part"] == "part2", "ΔF_min"]*n2/(n1+n2)).tolist()
+    F_min_bin = df_delta.loc[df_delta["#part"] == "binary", "ΔF_min"].values[0]/(n1+n2)  # Tomar único valor binario
     struc_part_min = []
     for i, part in enumerate(["part1", "part2"]):
         F_part = F_part1 if part == "part1" else F_part2  # Seleccionar la lista correcta
@@ -225,9 +244,9 @@ def delta_energy_F(dict_delta, cell, n1, n2):
 
 def delta_energy_US(dict_delta, US, cell_min, n1, n2):
     df_delta = pd.DataFrame.from_dict(dict_delta)
-    F_part1 = df_delta.loc[(df_delta["#part"] == "part1") & (df_delta["cell"] == cell_min[0]), f"{US}_min"].values[0]
-    F_part2 = df_delta.loc[(df_delta["#part"] == "part2") & (df_delta["cell"] == cell_min[1]), f"{US}_min"].values[0]
-    F_min_bin = df_delta.loc[df_delta["#part"] == "binary", f"{US}_min"].values[0]  # Tomar único valor binario
+    F_part1 = df_delta.loc[(df_delta["#part"] == "part1") & (df_delta["cell"] == cell_min[0]), f"{US}_min"].values[0]/(n1+n2)
+    F_part2 = df_delta.loc[(df_delta["#part"] == "part2") & (df_delta["cell"] == cell_min[1]), f"{US}_min"].values[0]/(n1+n2)
+    F_min_bin = df_delta.loc[df_delta["#part"] == "binary", f"{US}_min"].values[0]/(n1+n2)  # Tomar único valor binario
     DF = F_min_bin - F_part1*n1 - F_part2*n2
 
     return DF
@@ -236,9 +255,9 @@ def delta_energy_contrib(dict_contrib, File_name, cell_min, n1, n2):
     DF_calc = []
     df_contrib = pd.DataFrame.from_dict(dict_contrib)
     for i, F in enumerate(File_name):
-        F_part1 = df_contrib.loc[(df_contrib["#part"] == "part1") & (df_contrib["cell"] == cell_min[0]), f"{F}"].values[0]
-        F_part2 = df_contrib.loc[(df_contrib["#part"] == "part2") & (df_contrib["cell"] == cell_min[1]), f"{F}"].values[0]
-        F_min_bin = df_contrib.loc[df_contrib["#part"] == "binary", f"{F}"].values[0]  # Tomar único valor binario
+        F_part1 = df_contrib.loc[(df_contrib["#part"] == "part1") & (df_contrib["cell"] == cell_min[0]), f"{F}"].values[0]/(n1+n2)
+        F_part2 = df_contrib.loc[(df_contrib["#part"] == "part2") & (df_contrib["cell"] == cell_min[1]), f"{F}"].values[0]/(n1+n2)
+        F_min_bin = df_contrib.loc[df_contrib["#part"] == "binary", f"{F}"].values[0]/(n1+n2)  # Tomar único valor binario
 
         DF_calc.append(F_min_bin - F_part1*n1 - F_part2*n2)
 

scripts_python/core_program_export.py
@@ -96,6 +96,7 @@ while True:
 	check_extract = input("¿Quiere extraer los F.data? (s/n): ").strip().lower()
 	if check_extract in ["s", "si"]:
 		print("Extrayendo F.data...")
+		check_bcc = False
 		for gamma_folder in gamma_folder_list:
 			os.chdir(os.path.join(dir_origin,f"gamma_{gamma_folder}"))
 
@@ -135,12 +136,14 @@ while True:
 						k_aL_part = 1
 						if flag_reflexion_part == True:
 							k_aL_part = 2
-						process_principal_part(output_file, label_struc, R_np, delta_list, aL, k_aL_part, f_name)
+						process_principal_part(output_file, label_struc, R_np, delta_list, aL, k_aL_part, f_name, check_bcc)
 
 						os.chdir(os.path.join(dir_fuente[label], f"{label_struc}_ref"))
 						output_file = os.path.join(output_folder, f"{label}_references_{f_name}.csv")
 						base_folder = os.path.join(dir_fuente[label], f"{label_struc}_ref")
 						process_reference_part(output_file, base_folder, cell_part, label_struc, f_name)
+						if (label_struc == 'bcc' and label == 'part2'):
+							check_bcc = True
 
 			join_F_csv(output_folder, name_bin, True)
 			join_F_csv_ref(output_folder, name_bin)
@@ -206,12 +209,12 @@ for gamma_folder in gamma_folder_list:
 	for part in ["part1", "part2"]:
 		result_cell = []
 		for i, cell in enumerate(cell_part):
-			result_cell = estimate_part_F(part, cell, factor_aL_part[cell], n[part], k_part[cell], n1, n2, gen_curves_flag, k_aL_part)
+			result_cell = estimate_part_F(part, cell, factor_aL_part[cell], n[part], k_part[cell], gen_curves_flag, k_aL_part)
 			aL_min = result_cell[0]
 			F_part = result_cell[1]
 			aL_array = result_cell[2]
-			U = estimate_part_contrib(part, cell, factor_aL_part[cell], n[part], k_part[cell], n1, n2, F_U, aL_array, aL_min, gen_curves_flag, k_aL_part)
-			S = estimate_part_contrib(part, cell, factor_aL_part[cell], n[part], k_part[cell], n1, n2, F_ST, aL_array, aL_min, gen_curves_flag, k_aL_part)
+			U = estimate_part_contrib(part, cell, factor_aL_part[cell], n[part], k_part[cell], F_U, aL_array, aL_min, gen_curves_flag, k_aL_part)
+			S = estimate_part_contrib(part, cell, factor_aL_part[cell], n[part], k_part[cell], F_ST, aL_array, aL_min, gen_curves_flag, k_aL_part)
 
 			for i,key in enumerate(dict_delta):
 				list = [part,cell,aL_min,U,S,F_part]
@@ -271,6 +274,7 @@ if gen_curves_flag == True:
 os.chdir(dir_origin)
 
 gamma_value = []
+alpha_value = []
 DU_global = []
 DS_global = []
 DF_global = []
@@ -287,6 +291,8 @@ for gamma_folder in gamma_folder_list:
 		if j<3:
 			values = df.loc[df["#part"] == name, ["ΔU_min", "-ΔST_min", "ΔF_min"]]
 			gamma = df.loc[df["aL_min"] == "Global X", ["cell"]].iloc[0]
+			R2 = df.loc[df["#part"] == "R2 [nm]", ["cell"]].iloc[0]
+			alpha = R2/R1_np
 			if name != "binary":
 				DU, DS, DF = values.sort_values(by="ΔF_min").iloc[0]
 			else:
@@ -301,6 +307,7 @@ for gamma_folder in gamma_folder_list:
 		DF_values[j].append(DF)
 
 	gamma_value.append(gamma)
+	alpha_value.append(alpha)
 
 	os.chdir(dir_origin)
 
@@ -331,6 +338,22 @@ for i, (lista, F) in enumerate(zip(F_plot,["ΔU", "-TΔS", "ΔF"])):
 	plt.title(f"{name_bin}",fontsize=22,weight='bold')
 	plt.legend(fontsize=13)
 	plt.savefig(f'{final_output}/{name_bin}_results_{F}.png',format='png',dpi=300,bbox_inches='tight')
+
+F_plot = [DU_values[3],DS_values[3],DF_values[3]]
+for i, (lista, F) in enumerate(zip(F_plot,["ΔU", "-TΔS", "ΔF"])):
+	plt.figure(figsize=(8, 6))
+	plt.plot(alpha_value,F_plot[i],ls='none',marker='s',color='red',ms=7,label='MoltCF')
+
+	plt.axhline(0,ls='--',c='darkgray',zorder=-3)
+	plt.xticks(fontsize=14)
+	plt.yticks(fontsize=14)
+	plt.xlim(np.min(alpha_value)-0.05,np.max(alpha_value)+0.05)
+	plt.ylabel(y_label[i],fontsize=18)
+	plt.xlabel(r'$\alpha$',fontsize=18)
+	plt.title(f"{name_bin}",fontsize=22,weight='bold')
+	plt.legend(fontsize=13)
+	plt.savefig(f'{final_output}/{name_bin}_results_{F}_alpha.png',format='png',dpi=300,bbox_inches='tight')
+
 
 #####################################################
 

scripts_python/export_output.py
@@ -129,16 +129,21 @@ def process_reference_bin(output_file, dir_inicial, F, R, gamma_folder):
                         delta_value = delta.replace('_', '.')  
                         writer.writerow([label,R[label], delta_value, dimx, dimy, dimz, f_ref])
 
-def process_principal_part(output_file, label_struc,R_np, delta_list, aL, k_aL, F):
+def process_principal_part(output_file, label_struc,R_np, delta_list, aL, k_aL, F, check_bcc):
     structure = os.getcwd()
     if not os.path.isfile(output_file):
         with open(output_file, "w") as out_file:
             out_file.write("cell, radius [nm],delta,dimx,dimy,dimz,F_value\n")
+    if ("bcc" in structure and "part2" in structure) and check_bcc == False:
+        delta_list.append(0.26)
 
     for delta in delta_list:
         delta_folder = str(delta).replace('.', '_')
         round_value = int(np.round(float(aL/k_aL) / float(delta)))
-        dims = [round_value - 1, round_value, round_value + 1]
+        if not delta == 0.26:
+            dims = [round_value - 1, round_value, round_value + 1]
+        else:
+            dims = [round_value]
         
         for j in dims:
             folder_name = f"delta_{delta_folder}_dim_{j}"

scripts_python/function_part.py
@@ -71,12 +71,13 @@ def path_carpeta(folder_init,n):
         x = os.path.dirname(x)
     return x
 
-def process_principal_part(reference_DEF, delta_list, aL, tosubmit, dir_fuente, k_aL):
+def process_principal_part(reference_DEF, delta_list, aL, tosubmit, dir_fuente, k_aL, check_bcc):
     structure = os.getcwd()
     DEF =  os.path.join(structure, "DEFINITIONS.txt")
     lines = read_DEF(DEF)
-    if ("bcc" and "part2") in structure:
+    if ("bcc" in structure and "part2" in structure) and check_bcc == False:
         delta_list.append(0.26)
+
     for delta in delta_list:
         round_value = int(np.round(float(aL/k_aL) / float(delta)))
         if not delta == 0.26: