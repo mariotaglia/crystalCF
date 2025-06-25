32b6c3d7cb24b05167b360c12b0ac067a6524758
santiago <santiagofbonoli@gmail.com>
2025-06-24 21:43:22 -0300

    Added code for multiple chain lengt for binary systems

--- scripts_python/references/aL_min_bcc.py
+++ scripts_python/references/aL_min_bcc.py
@@ -3,11 +3,12 @@ import math
 import sys
 import os
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     raise ValueError("Debes proporcionar R_np como argumento")
 
 R = float(sys.argv[1])  # Convertir argumento a float
 l_pol = float(sys.argv[2])  # Convertir argumento a float
+sigma = float(sys.argv[3])
 
 sigma = 5.85 #1/nm^2
 pi = math.pi
